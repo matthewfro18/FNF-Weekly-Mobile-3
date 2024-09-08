4c7adc135f0e0dd7fb02c60769453eff461f8236
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-09-08 13:16:18 +0800

    Update MenuCharacter.hx

--- source/gameObjects/MenuCharacter.hx
+++ source/gameObjects/MenuCharacter.hx
@@ -10,7 +10,9 @@ import openfl.utils.Assets;
 import haxe.Json;
 import haxe.format.JsonParser;
 import meta.data.*;
+#if desktop
 import meta.data.Discord.DiscordClient;
+#end
 
 typedef MenuCharacterFile = {
 	var image:String;
